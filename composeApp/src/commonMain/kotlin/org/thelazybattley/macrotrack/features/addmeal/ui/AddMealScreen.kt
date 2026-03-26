0cac8710cddb7476ce5ff54740acc00bd6dd91c4
dellosaneil <dellosaneil@gmail.com>
2026-03-26 18:27:22 +0800

    created UI for Macros in Add meal

--- composeApp/src/commonMain/kotlin/org/thelazybattley/macrotrack/features/addmeal/ui/AddMealScreen.kt
+++ composeApp/src/commonMain/kotlin/org/thelazybattley/macrotrack/features/addmeal/ui/AddMealScreen.kt
@@ -18,12 +18,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import macrotrack.composeapp.generated.resources.Res
 import macrotrack.composeapp.generated.resources.add_meal
+import macrotrack.composeapp.generated.resources.carbs
+import macrotrack.composeapp.generated.resources.fat
+import macrotrack.composeapp.generated.resources.kcal_text
+import macrotrack.composeapp.generated.resources.protein
+import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import org.thelazybattley.macrotrack.domain.model.MealType
@@ -70,6 +76,77 @@ private fun AddMealScreen(
             modifier = Modifier.fillMaxWidth(), callbacks = callbacks,
             selectedMealType = viewState.selectedMealType
         )
+        MacrosDetail(modifier = Modifier.fillMaxWidth())
+    }
+}
+
+
+@Composable
+private fun MacrosDetail(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(space = 8.dp)
+    ) {
+        MacroCard(
+            modifier = Modifier.weight(weight = 1f),
+            value = "32",
+            macro = Res.string.kcal_text,
+            textColor = colors.black,
+        )
+        MacroCard(
+            modifier = Modifier.weight(weight = 1f),
+            value = "21g",
+            macro = Res.string.protein,
+            textColor = colors.deepBlue
+        )
+        MacroCard(
+            modifier = Modifier.weight(weight = 1f),
+            value = "11g",
+            textColor = colors.green,
+            macro = Res.string.carbs
+        )
+        MacroCard(
+            modifier = Modifier.weight(weight = 1f),
+            value = "1g",
+            macro = Res.string.fat,
+            textColor = colors.orange
+        )
+    }
+}
+
+@Composable
+private fun MacroCard(
+    modifier: Modifier = Modifier,
+    value: String,
+    macro: StringResource,
+    textColor: Color
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = colors.offWhite
+        ),
+        shape = RoundedCornerShape(size = 12.dp),
+        modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(space = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier.padding(top = 6.dp),
+                text = value,
+                style = typography.bold15,
+                color = textColor
+            )
+            Text(
+                text = stringResource(resource = macro),
+                color = colors.gray,
+                style = typography.regular10,
+                modifier = Modifier.padding(bottom = 6.dp)
+            )
+        }
+
     }
 }
 
