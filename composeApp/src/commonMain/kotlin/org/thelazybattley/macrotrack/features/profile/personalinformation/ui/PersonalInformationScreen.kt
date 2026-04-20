174de39b0c346f13a23eb65b13a540fe5aabd093
dellosaneil <dellosaneil@gmail.com>
2026-04-20 14:18:10 +0800

    created UI for activityLevel

--- composeApp/src/commonMain/kotlin/org/thelazybattley/macrotrack/features/profile/personalinformation/ui/PersonalInformationScreen.kt
+++ composeApp/src/commonMain/kotlin/org/thelazybattley/macrotrack/features/profile/personalinformation/ui/PersonalInformationScreen.kt
@@ -1,5 +1,6 @@
 package org.thelazybattley.macrotrack.features.profile.personalinformation.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.Orientation
@@ -10,32 +11,44 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import macrotrack.composeapp.generated.resources.Res
+import macrotrack.composeapp.generated.resources.activity_level_camel
 import macrotrack.composeapp.generated.resources.age
 import macrotrack.composeapp.generated.resources.cm
 import macrotrack.composeapp.generated.resources.empty
 import macrotrack.composeapp.generated.resources.female
 import macrotrack.composeapp.generated.resources.height
+import macrotrack.composeapp.generated.resources.ic_chevron_left
 import macrotrack.composeapp.generated.resources.male
 import macrotrack.composeapp.generated.resources.personal_information
 import macrotrack.composeapp.generated.resources.sex
 import macrotrack.composeapp.generated.resources.yrs
 import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
+import org.thelazybattley.macrotrack.domain.model.ActivityLevel
+import org.thelazybattley.macrotrack.domain.model.Goal
+import org.thelazybattley.macrotrack.domain.model.UserDetails
 import org.thelazybattley.macrotrack.domain.model.UserSex
+import org.thelazybattley.macrotrack.domain.model.descriptionRes
+import org.thelazybattley.macrotrack.domain.model.iconRes
+import org.thelazybattley.macrotrack.domain.model.titleRes
 import org.thelazybattley.macrotrack.features.navigation.AppPadding
 import org.thelazybattley.macrotrack.features.profile.personalinformation.PersonalInformationCallbacks
 import org.thelazybattley.macrotrack.features.profile.personalinformation.PersonalInformationViewModel
@@ -129,8 +142,79 @@ private fun PersonalInformationScreen(
             value = viewState.userDetails?.height.toString(),
             suffixText = Res.string.cm
         )
+        if (viewState.userDetails?.activityLevel != null) {
+            ActivityLevelUi(
+                modifier = Modifier.fillMaxWidth(),
+                activityLevel = viewState.userDetails.activityLevel
+            )
+        }
     }
 }
+
+@Composable
+private fun ActivityLevelUi(
+    modifier: Modifier = Modifier,
+    activityLevel: ActivityLevel
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(space = 8.dp)
+    ) {
+        Text(
+            text = stringResource(resource = Res.string.activity_level_camel),
+            color = colors.mediumGray,
+            style = typography.bold10
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = colors.white,
+                    shape = RoundedCornerShape(size = 8.dp)
+                )
+                .border(
+                    width = 1.dp,
+                    color = colors.lightGray,
+                    shape = RoundedCornerShape(size = 8.dp)
+                )
+                .padding(all = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(space = 16.dp)
+        ) {
+            Image(
+                modifier = Modifier
+                    .background(color = colors.paleBlue, shape = RoundedCornerShape(size = 8.dp))
+                    .padding(all = 8.dp)
+                    .size(size = 16.dp),
+                painter = painterResource(resource = activityLevel.iconRes()),
+                contentDescription = null
+            )
+            Column(
+                modifier = Modifier.weight(weight = 1f),
+                verticalArrangement = Arrangement.spacedBy(space = 8.dp)
+            ) {
+                Text(
+                    text = stringResource(resource = activityLevel.titleRes()),
+                    color = colors.black,
+                    style = typography.bold14,
+                )
+                Text(
+                    text = stringResource(resource = activityLevel.descriptionRes()),
+                    color = colors.mediumGray,
+                    style = typography.regular11
+                )
+            }
+
+            Icon(
+                modifier = Modifier.size(size = 16.dp).rotate(degrees = 180f),
+                painter = painterResource(resource = Res.drawable.ic_chevron_left),
+                contentDescription = null,
+                tint = colors.mediumGray
+            )
+        }
+    }
+}
+
 
 @Composable
 private fun PersonalInfoValues(
@@ -213,7 +297,16 @@ private fun PreviewPersonalInformationScreen() {
     MacroTrackTheme {
         PersonalInformationScreen(
             modifier = Modifier.fillMaxWidth(),
-            viewState = PersonalInformationViewState(),
+            viewState = PersonalInformationViewState(
+                userDetails = UserDetails(
+                    weight = 60.0,
+                    age = 20,
+                    height = 170.0,
+                    sex = UserSex.MALE,
+                    activityLevel = ActivityLevel.LIGHTLY_ACTIVE,
+                    goal = Goal.MAINTAIN_WEIGHT
+                )
+            ),
             callbacks = PersonalInformationCallbacks.default()
         )
     }
